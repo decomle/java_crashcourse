52de41f9378dc43520fb4c5d762984ab5defa77f
Dat Phung <thedat230589@gmail.com>
2026-01-13 14:29:53 +0700

    init commit

--- relearn/src/main/java/com/decomle/relearn/HelloWorldController.java
+++ relearn/src/main/java/com/decomle/relearn/HelloWorldController.java
@@ -1,4 +1,12 @@
 package com.decomle.relearn;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+        return "Hello world test";
+    }
 }
---
+++ relearn/src/main/java/com/decomle/relearn/RelearnApplication.java
@@ -0,0 +1,13 @@
+package com.decomle.relearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelearnApplication.class, args);
+	}
+
+}
